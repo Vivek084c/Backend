abaf1d85292c2eb083cbba965ae5d642b1b684ac
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-08-05 23:08:19 +0530

    os and path module

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
-    // modules
-// const vivek = "vivek"
-// const  abhi = "abhi"
-// const sayHi = (name)=>{
-//     console.log(`hellow there ${name}`);
+//     // modules
+// // const vivek = "vivek"
+// // const  abhi = "abhi"
+// // const sayHi = (name)=>{
+// //     console.log(`hellow there ${name}`);
+// // }
+
+//     // accessing the variables
+// const names = require('./4_names');
+// const sayHi = require('./5_utils')
+// console.log(names)
+
+
+// const data = require('./6_alternative_flavours.js')
+// console.log("the app data starts here")
+// console.log(data)
+
+// console.log("the log data ends here")
+
+// // sayHi('vivek')
+// // sayHi(names.vivek)
+// // sayHi(names.abhi)
+
+// require('./7_mind_grenade.js')
+
+
+
+// // working with the os module------------
+
+// const os = require('os')
+
+// //info about current user
+// const user = os.userInfo()
+// console.log(`the user info are as follow `)
+// console.log(user)
+
+
+// //method to return the system up time
+// console.log(`the system up time ${os.uptime} seconds`)
+
+// const currentOS = {
+//     name:os.type(),
+//     release:os.release(),
+//     totalMem:os.totalmem(),
+//     freeMem:os.freemem(),
 // }
 
-    // accessing the variables
-const names = require('./4_names');
-const sayHi = require('./5_utils')
-console.log(names)
+// console.log(currentOS)
 
-
-const data = require('./6_alternative_flavours.js')
-console.log("the app data starts here")
-console.log(data)
-
-console.log("the log data ends here")
-
-// sayHi('vivek')
-// sayHi(names.vivek)
-// sayHi(names.abhi)
-
-require('./7_mind_grenade.js')
