7d5f933fede61ae43f5ee93e3e54ef25ffc837c1
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-08-06 12:58:16 +0530

    file reading and writing

--- app.js
+++ app.js
@@ -47,3 +47,18 @@
 
 // console.log(currentOS)
 
+const {readFile, writeFile} = require('fs');
+
+readFile('./content/first.txt','utf8',(err, result)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(result);
+
+})
+
+
+
+
+
