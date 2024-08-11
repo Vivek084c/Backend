03f9a96e18c61e9ceb89656dbda1e12bf4af8707
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-08-11 11:17:56 +0530

    sync and async calls

--- 10_fs_sync.js
+++ 10_fs_sync.js
@@ -14,3 +14,48 @@ console.log('done with this task');
 console.log('starting the next tast');
 
 
+
+const {readFile} = require('fs')
+
+console.log('started a first task')
+// check the file path
+readFile('./content/first.txt', 'utf8', (err, result)=>{
+    if (err){
+        console.log(err)
+        return
+    }
+    console.log(result)
+    console.log('compiled the fist task')
+})
+
+console.log('started the second task')
+
+
+
+// started operating system process
+console.log('first')
+setTimeout(() => {
+    console.log('second')
+}, 0);
+console.log('third')
+//completed and excited operating system
+
+
+setInterval(() => {
+    console.log('hellow world')
+}, 2000);
+console.log('i will run first')
+// process stays alive until kill the process with ctrl + c, unexpected error
+
+const http = require('http')
+
+const server = http.createServer((req, res)=> {
+    console.log('request event')
+    res.end('hellow world')
+})
+
+server.listen(8888, ()=>{
+    console.log('server is listening to the port')
+})
+
+
--- 11_fs_async.js
+++ 11_fs_async.js
@@ -27,3 +27,31 @@ readFile('./content/first.txt','utf8',(err, result)=>{
 
 })
 console.log('starting the next task')
+
+
+const http  = require('http');
+
+const server = http.createServer((req, res)=>{
+    
+if(req.url == '/'){
+    //its a home page
+    res.end('welcome to home page')
+    return;
+}
+if(req.url == '/about'){
+    //its a home page
+    res.end('welcome to about page')
+    return;
+}
+else{
+    res.end('error ocured')
+    return;
+}
+
+
+})
+
+server.listen(8888, ()=>{
+    console.log('server is litening on port 8888')
+})
+
