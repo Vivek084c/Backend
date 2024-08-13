a0704bb6dce24df11ccce78b46a8d9e41a85c103
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-08-13 14:51:27 +0530

    server and html appending s

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 const http = require('http')
 
+const {readFileSync} = require('fs');
+
+// get all the files
+const hopePage = readFileSync('./express_tutorials/navbar-app/index.html')
+const homeStyle = readFileSync('./express_tutorials/navbar-app/styles.css')
+const homeImage = readFileSync('./express_tutorials/navbar-app/logo.svg')
+const homeLogic = readFileSync('./express_tutorials/navbar-app/browser-app.js')
+
 const server = http.createServer((req, res)=>{
     // console.log(req.url)
     // console.log(`user has hit the server`)
 
     const url = req.url
+    console.log(url)
 
     // home page
     if (url == '/'){
         res.writeHead(200, {'content-type':'text/html'})
-        res.write('<h1>welcome to home page</h1>')
+        res.write(hopePage)
         res.end()
-    } // about page
-    else if (url == '/about'){
-        res.writeHead(200, {'content-type':'text/html'})
-        res.write('<h1>welcome to about page</h1>')
+    } // styles
+    else if (url == '/styles.css'){
+        res.writeHead(200, {'content-type':'text/css'})
+        res.write(homeStyle)
+        res.end()
+    }// image file
+    else if (url == '/logo.svg'){
+        res.writeHead(200, {'content-type':'image/svg+xml'})
+        res.write(homeImage)
+        res.end()
+    }// logic
+    else if (url == '/browser-app.js'){
+        res.writeHead(200, {'content-type':'text/javascript'})
+        res.write(homeLogic)
         res.end()
     }// 404
     else{
