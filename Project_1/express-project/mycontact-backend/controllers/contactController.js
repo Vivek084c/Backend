e8788c2334cc3b49fc6eeaaf57366998b184d2cb
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-08-17 18:10:15 +0530

    server routing and error handling

--- Project_1/express-project/mycontact-backend/controllers/contactController.js
+++ Project_1/express-project/mycontact-backend/controllers/contactController.js
@@ -1,35 +1,41 @@
 //@desc Get all contacts
 //@route Get /api/contact
 //@access public
-const getContacts = (req, res)=>{
+const getContacts = async (req, res)=>{
     res.status(200).json({message: 'get all contact'});
 };
 
 //@desc Create new contacts
 //@route POST /api/contact
 //@access public
-const createContact = (req, res)=>{
+const createContact = async (req, res)=>{
+    console.log("The requrest body is :", req.body)
+    const {name, email, contact}  = req.body;
+    if (!name || !email || !contact){
+        res.status(401);
+        throw new Error("All Fields are necessary");
+    }
     res.status(201).json({message: 'post contact'});
 };
 
 //@desc Get contacts
 //@route GET /api/contact/:id
 //@access public
-const getContact = (req, res)=>{
+const getContact = async (req, res)=>{
     res.status(200).json({message:  `got contact for ${req.params.id}`});
 };
 
 //@desc update contacts
 //@route PUT /api/contact/:id
 //@access public
-const updateContact = (req, res)=>{
+const updateContact = async (req, res)=>{
     res.status(200).json({message: `update contact for ${req.params.id}`});
 };
 
 //@desc delete contacts
 //@route DELETE /api/contact/:id
 //@access public
-const deleteContact = (req, res)=>{
+const deleteContact = async (req, res)=>{
     res.status(200).json({message:  `delete contact for ${req.params.id}`});
 };
 
--- Project_1/express-project/mycontact-backend/server.js
+++ Project_1/express-project/mycontact-backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const errorHandler = require('./middelware/errorHandle');
 const dotenv = require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT  || 8877;
-
+//midelware to get the body from request body
+app.use(express.json())
 app.use('/api/contact', require('./routes/contactRoutes'))
+// middelware to handle the error 
+app.use(errorHandler)
+
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
